4dea97c5dcf34d394700a9e3713d45546fa49689
tommaso-merlini <tommaso.merlini.2004@gmail.com>
2022-10-02 16:22:56 +0200

    hotfix

--- src/app.ts
+++ src/app.ts
@@ -5,21 +5,12 @@ const app = express();
 const port = 3000;
 const prisma = new PrismaClient();
 
-async function main() {
-  // Connect the client
-  await prisma.$connect();
-  // ... you will write your Prisma Client queries here
-}
-
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+prisma.post.create({
+  data: {
+    title: "2",
+    body: "2",
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World");
