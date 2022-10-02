70f907ac4470debad5ee117f6dae500407091463
tommaso-merlini <tommaso.merlini.2004@gmail.com>
2022-10-02 16:04:22 +0200

    installing setting up prisma

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,25 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+
 const app = express();
 const port = 3000;
+const prisma = new PrismaClient();
+
+async function main() {
+  // Connect the client
+  await prisma.$connect();
+  // ... you will write your Prisma Client queries here
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello World");
