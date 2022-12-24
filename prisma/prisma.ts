2cd4d84916d77904c96c891df7edb9dc694b17b7
tommaso-merlini <tommaso.merlini.2004@gmail.com>
2022-12-25 00:37:31 +0100

    dynamic database url on prisma schema

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,8 +1,23 @@
 import { PrismaClient } from "@prisma/client";
 import chalk from "chalk";
+require("dotenv").config();
+
+let database_url;
+
+if(process.env.NODE_ENV === "production") {
+  database_url = process.env.PROD_DATABSE_URL
+} else {
+  database_url = process.env.DEV_DATABSE_URL
+}
 
 export type Prisma = PrismaClient;
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+    datasources: {
+        db: {
+          url: database_url,
+        },
+      },
+});
 
 console.log(chalk.bgGreen.black("Prisma client initialized"));
