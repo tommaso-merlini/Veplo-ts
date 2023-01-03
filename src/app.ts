c2e2613e5be1cfe429463921bd011ea6d70792fb
tommaso-merlini <tommaso.merlini.2004@gmail.com>
2023-01-03 17:20:56 +0100

    bucket upload

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import rateLimit from "express-rate-limit";
 import { GraphQLError } from "graphql";
 import initMongoose from "../mongoose/initMongoose";
 var mongoose = require("mongoose");
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 require("dotenv").config();
 
 process.on("uncaughtException", function (err) {
@@ -75,6 +76,45 @@ function startServer() {
 
   // If the Node process ends, close the Mongoose connection
   process.on("SIGINT", gracefulExit).on("SIGTERM", gracefulExit);
+
+  // Step 2: The s3Client function validates your request and directs it to your Space's specified endpoint using the AWS SDK.
+  const s3Client = new S3Client({
+    endpoint: "htps://spaceprova1.fra1.digitaloceanspaces.com", // Find your endpoint in the control panel, under Settings. Prepend "https://".
+    forcePathStyle: false, // Configures to use subdomain/virtual calling format.
+    region: "eu-central-1", // Must be "us-east-1" when creating new Spaces. Otherwise, use the region in your endpoint (e.g. nyc3).
+    credentials: {
+      accessKeyId: "DO00ED2V82BGGG2Z37LE", // Access key pair. You can create access key pairs using the control panel or API.
+      secretAccessKey: "SsjLGakwxMXh0eEQA8l+XVadKv9tgNyPkFcEkEd+yUg", // Secret access key defined through an environment variable.
+    },
+  });
+
+  // Step 3: Define the parameters for the object you want to upload.
+  const params = {
+    Bucket: "spaceprova1", // The path to the directory you want to upload the object to, starting with your Space name.
+    Key: "hello-world.txt", // Object key, referenced whenever you want to access this file later.
+    Body: "Hello, World!", // The object's contents. This variable is an object, not a string.
+    ACL: "private", // Defines ACL permissions, such as private or public.
+    Metadata: {
+      // Defines metadata tags.
+      "x-amz-meta-my-key": "your-value",
+    },
+  };
+
+  // Step 4: Define a function that uploads your object using SDK's PutObjectCommand object and catches any errors.
+  const uploadObject = async () => {
+    try {
+      const data = await s3Client.send(new PutObjectCommand(params));
+      console.log(
+        "Successfully uploaded object: " + params.Bucket + "/" + params.Key
+      );
+      return data;
+    } catch (err) {
+      console.log("Error", err);
+    }
+  };
+
+  // Step 5: Call the uploadObject function.
+  uploadObject();
 }
 
 startServer();
