18c59a2b6236ab6b90c3294ee857ac109be6e218
tommaso-merlini <tommaso.merlini.2004@gmail.com>
2023-01-07 13:53:15 +0100

    removed compression

--- src/app.ts
+++ src/app.ts
@@ -11,26 +11,20 @@ import { PutObjectCommand } from "@aws-sdk/client-s3";
 import fs from "fs";
 require("dotenv").config();
 import { graphqlUploadExpress } from "graphql-upload";
-import compression from "compression";
 
 process.on("uncaughtException", function (err) {
   console.error(err);
   console.log("Node NOT Exiting...");
 });
 
-const limiter = rateLimit({
-  windowMs: 10 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});
+// const limiter = rateLimit({
+//   windowMs: 10 * 1000, // 15 minutes
+//   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+//   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+//   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+// });
 
 const app = express();
-app.use(
-  compression({
-    options: ["zlib"],
-  })
-);
 const port = process.env.PORT || 3000;
 
 function startServer() {
@@ -42,7 +36,7 @@ function startServer() {
     await apolloserver.start();
     await apolloserver.applyMiddleware({ app });
 
-    app.use(limiter);
+    // app.use(limiter);
 
     app.get("/", (req, res) => {
       res.send("Hello World");
