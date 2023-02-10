b8b90490b541d9a273abb594cab87da079aadeab
tommaso-merlini <tommaso.merlini.2004@gmail.com>
2023-02-10 23:03:14 +0100

    change space accessKeyId

--- spaces/s3Client.ts
+++ spaces/s3Client.ts
@@ -7,7 +7,7 @@ const s3Client = new S3Client({
   forcePathStyle: false, // Configures to use subdomain/virtual calling format.
   region: "eu-central-1", // Must be "us-east-1" when creating new Spaces. Otherwise, use the region in your endpoint (e.g. nyc3).
   credentials: {
-    accessKeyId: "DO00ED2V82BGGG2Z37LE", // Access key pair. You can create access key pairs using the control panel or API.
+    accessKeyId: "DO00JYZX3JZPBZTTXY96", // Access key pair. You can create access key pairs using the control panel or API.
     secretAccessKey: process.env.BUCKET_SECRET, // Secret access key defined through an environment variable.
   },
 });
