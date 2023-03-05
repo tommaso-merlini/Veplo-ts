86d366ae22644e42e368dcb5f35fc4c10964e2da
tommaso-merlini <tommaso.merlini.2004@gmail.com>
2023-03-05 17:34:46 +0100

    cart schema mongodb

---
+++ src/schemas/Cart.model.ts
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const CartSchema = new mongoose.Schema({
+  userId: { type: mongoose.Types.ObjectId, required: true },
+  total: { type: Number, required: true },
+  status: { type: String, required: true },
+  shop: {
+    id: { type: mongoose.Types.ObjectId, required: true },
+    name: { type: String, required: true },
+    city: { type: String, required: true },
+  },
+  productsVariations: [
+    {
+      id: { type: mongoose.Types.ObjectId, required: true },
+      photo: { type: String, required: true },
+      name: { type: String, required: true },
+      price: {
+        v1: {
+          type: Number,
+          required: true,
+        },
+        v2: {
+          type: Number,
+          required: false,
+        },
+        discountPercentage: { type: Number, required: false },
+      },
+      quantity: { type: Number, required: true },
+      color: { type: String, required: true },
+      size: { type: String, required: true },
+      status: { type: String, required: true },
+    },
+  ],
+});
+
+const Cart = mongoose.model("cart", CartSchema);
+export default Cart;
