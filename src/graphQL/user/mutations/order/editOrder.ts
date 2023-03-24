6be70082ef5d7b7353d9228e4cd8e00837bd9930
tommaso-merlini <tommaso.merlini.2004@gmail.com>
2023-03-24 20:15:17 +0100

    editOrder admin roles

--- src/graphQL/user/mutations/order/editOrder.ts
+++ src/graphQL/user/mutations/order/editOrder.ts
@@ -4,6 +4,7 @@ import checkFirebaseErrors from "../../../../controllers/checkFirebaseErrors";
 import orderById from "../../../../controllers/queries/orderById";
 import lodash from "lodash";
 import Order from "../../../../schemas/Order.model";
+import customError from "../../../../controllers/errors/customError";
 
 export const editOrder = async (
   _,
@@ -21,7 +22,23 @@ export const editOrder = async (
     token = {
       mongoId: "6410a5686c8721c863f3d1d9",
       isBusiness: "true",
+      isAdmin: true,
     };
+  }
+
+  //if the user is not an amdin
+  if (token.isAdmin !== true) {
+    if (options.url != null) {
+      //can't modify the url
+      delete options.url;
+    }
+    if (options.courier == null || options.code == null) {
+      customError({
+        code: "400",
+        path: "input",
+        message: "you need to input the code and the courier",
+      });
+    }
   }
 
   const order = await orderById(id);
@@ -33,6 +50,8 @@ export const editOrder = async (
   const orderShipping = order.shipping;
 
   const mergedShipping = lodash.merge(orderShipping, options);
+
+  console.log(mergedShipping);
 
   await Order.updateOne(
     { _id: id },
