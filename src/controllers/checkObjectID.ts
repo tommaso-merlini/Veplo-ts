b647ea7102ec70832094fbaadf4a38619f8cc8a9
tommaso-merlini <tommaso.merlini.2004@gmail.com>
2023-04-02 14:59:41 +0200

    id.match hotfix

--- src/controllers/checkObjectID.ts
+++ src/controllers/checkObjectID.ts
@@ -1,14 +1,15 @@
 const checkObjectID = (id) => {
-    if (id.match(/^[0-9a-fA-F]{24}$/)) {
-    } else {
-      throw Object.assign(new Error("Error"), {
-        extensions: {
-          customCode: "422",
-          customPath: "id",
-          customMessage: "the id provided is not a valid ObjectID",
-        },
-      });
-    }
-}
+  id = String(id);
+  if (id.match(/^[0-9a-fA-F]{24}$/)) {
+  } else {
+    throw Object.assign(new Error("Error"), {
+      extensions: {
+        customCode: "422",
+        customPath: "id",
+        customMessage: "the id provided is not a valid ObjectID",
+      },
+    });
+  }
+};
 
 export default checkObjectID;
