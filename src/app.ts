20d2f8b4acf99ffefedc9a66ffa42ce2d91c8798
tommaso-merlini <tommaso.merlini.2004@gmail.com>
2023-04-16 22:09:29 +0200

    cors in prod

--- src/app.ts
+++ src/app.ts
@@ -49,6 +49,21 @@ process.on("uncaughtException", function (err) {
 const app = express();
 const port = process.env.PORT || 3000;
 const appId = generateCode();
+
+if (process.env.NODE_ENV === "production") {
+  const whitelist = ["https://www.veplo.it"];
+  const corsOptions = {
+    origin: function (origin: any, callback: any) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback("Cors Error");
+      }
+    },
+  };
+  app.use(cors(corsOptions));
+}
+
 // const numCpus = os.cpus().length;
 let endpointSecretCheckout: string;
 let endpointSecretAccount: string;
