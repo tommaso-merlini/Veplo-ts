43557b01fc0dcc6e0a16c47b2bb4c1adffbe7709
tommaso-merlini <tommaso.merlini.2004@gmail.com>
2023-04-23 21:26:01 +0200

    updated checkContants

--- src/controllers/checkConstants.ts
+++ src/controllers/checkConstants.ts
@@ -1,191 +1,45 @@
-import {
-  EditLotsInput,
-  ProductVariationInput,
-} from "src/graphQL/types/types.js";
-import {
-  clothes_sizes,
-  constants,
-  shoes_sizes,
-} from "../../constants/constants.js";
+import { allFilters, colors } from "../../constants/filters.js";
 
 const checkConstants = (obj: any, is: String) => {
-  if (is !== "product" && is !== "shop") {
-    throw new Error("is can only be 'shop' or 'product");
+  const colorValues = [];
+  const sizeValues = [];
+  for (let variation of obj.variations) {
+    colorValues.push(variation.color);
+    for (let lot of variation.lots) {
+      sizeValues.push(lot.size);
+    }
   }
+  const checkableObject = {
+    ...obj.info,
+    colors: colorValues,
+    sizes: sizeValues,
+  };
 
-  if (is === "product") {
-    const product = obj;
-
-    let macroCategoryIndex: number = 0;
-    let macroCategory;
-
-    //---CHECK GENDER
-    if (product.info.gender !== "m" && product.info.gender !== "f") {
-      throw new Error(`gender deve essere 'm' o 'f'`);
-    }
-
-    //---CHECK MACRO-iCATEGORY
-    if (product.info.gender === "m") {
-      for (let i = 0; i < constants.genders.uomo.abbigliamento.length; i++) {
-        if (
-          product.info.macroCategory ===
-          constants.genders.uomo.abbigliamento[i]?.name
-        ) {
-          macroCategoryIndex = i;
+  for (const property in checkableObject) {
+    for (let filter of allFilters) {
+      if (Object.keys(filter)[0] === property) {
+        if (Array.isArray(checkableObject[property])) {
+          for (let value of checkableObject[property]) {
+            if (!filter[property as keyof typeof filter].includes(value)) {
+              throw new Error(
+                `error while checking ${property}, value ${value} does not exist in ${property} list`
+              );
+            }
+          }
+        } else {
+          if (
+            !filter[property as keyof typeof filter].includes(
+              checkableObject[property]
+            )
+          ) {
+            throw new Error(
+              `error while checking ${property}, value ${checkableObject[property]} does not exist in ${property} list`
+            );
+          }
+          break;
         }
       }
-
-      macroCategory = constants.genders.uomo.abbigliamento[macroCategoryIndex];
-      if (macroCategoryIndex === null || macroCategoryIndex === undefined) {
-        throw new Error(
-          `la category ${product.info.macroCategory} non e' una category accetata`
-        );
-      }
     }
-
-    if (product.info.gender === "f") {
-      for (let i = 0; i < constants.genders.donna.abbigliamento.length; i++) {
-        if (
-          product.info.macroCategory ===
-          constants.genders.donna.abbigliamento[i]?.name
-        ) {
-          macroCategoryIndex = i;
-        }
-      }
-
-      macroCategory = constants.genders.donna.abbigliamento[macroCategoryIndex];
-      if (macroCategoryIndex === null || macroCategoryIndex === undefined) {
-        throw new Error(
-          `la category ${product.info.macroCategory} non e' una category accetata`
-        );
-      }
-    }
-
-    //---CHECK COLORS
-    const areColorsOk = product.variations.every(
-      (variation: ProductVariationInput) =>
-        constants.colors.includes(variation.color)
-    );
-
-    if (!areColorsOk)
-      throw new Error(
-        `i colori che hai scelto non vanno bene, ecco la lista dei colori accettati: ${constants.colors}`
-      );
-
-    //---CHECK SIZES
-    product.variations.forEach((variation: ProductVariationInput) => {
-      variation.lots.forEach((lot: EditLotsInput) => {
-        if (
-          !clothes_sizes.includes(lot.size) &&
-          !shoes_sizes.includes(lot.size)
-        ) {
-          throw new Error(
-            `la taglia ${lot.size}, del prodotto di colore ${variation.color}, non e' una taglia accettata`
-          );
-        }
-      });
-    });
-
-    //---CHECK BRAND
-    const isBrandOk = constants.brands.includes(product.info.brand);
-    if (!isBrandOk)
-      throw new Error(
-        `il brand che hai scelto non e' supportato, lista dei brand supportati: ${constants.brands}`
-      );
-
-    //---CHECK MICRO-CATEGORY
-    const isMicroCategoryOk = macroCategory?.types.includes(
-      product.info.microCategory
-    );
-
-    if (!isMicroCategoryOk) {
-      throw new Error(
-        `micro-category ${product.info.microCategory} non e' accetata per macro-category ${macroCategory?.name}, micro-categories accetate per ${macroCategory?.name}: ${macroCategory?.types}`
-      );
-    }
-
-    //---CHECK Fit
-    const isFitOk = constants.fits.includes(product.info.fit);
-    if (!isFitOk) throw new Error(`il fit che hai scelto non e' supportato`);
-
-    //---CHECK Traits
-    const areTraitsOk = product.info.traits.every((trait: string) =>
-      constants.traits.includes(trait)
-    );
-
-    if (!areTraitsOk)
-      throw new Error(
-        `I tratti distintivi che hai scelto non vanno bene, lista di tratti accettati: ${constants.traits}`
-      );
-  } else {
-    //   const shop = obj;
-    //   //---CHECK GENDER
-    //   if (
-    //     shop.gender[0] !== "m" &&
-    //     shop.gender[0] !== "f" &&
-    //     shop.gender[1] !== "m" &&
-    //     shop.gender[1] !== "f"
-    //   ) {
-    //     throw new Error(`gender deve essere 'm' o 'f'`);
-    //   }
-    //   for (let l = 0; l < shop.gender.length; l++) {
-    //     if (shop.gender[l] === "mM") {
-    //       //cycles trough the shop macrocategories
-    //       for (
-    //         let macroCategoryIndex = 0;
-    //         macroCategoryIndex < shop.macroCategories.length;
-    //         macroCategoryIndex++
-    //       ) {
-    //         let macroCategory = shop.macroCategories[macroCategoryIndex];
-    //         let ok = false;
-    //         for (
-    //           let i = 0;
-    //           i < constants.genders.uomo.abbigliamento.length;
-    //           i++
-    //         ) {
-    //           console.log(constants.genders.uomo.abbigliamento[i].name);
-    //           if (
-    //             macroCategory === constants.genders.uomo.abbigliamento[i].name
-    //           ) {
-    //             ok = true;
-    //           }
-    //         }
-    //         if (!ok) {
-    //           throw new Error(
-    //             `la category ${macroCategory} non e' una category accetata`
-    //           );
-    //         }
-    //       }
-    //     }
-    //     if (shop.gender[l] === "F") {
-    //       //cycles trough the shop macrocategories
-    //       for (
-    //         let macroCategoryIndex = 0;
-    //         macroCategoryIndex < shop.macroCategories.length;
-    //         macroCategoryIndex++
-    //       ) {
-    //         let macroCategory = shop.macroCategories[macroCategoryIndex];
-    //         let ok = false;
-    //         for (
-    //           let i = 0;
-    //           i < constants.genders.donna.abbigliamento.length;
-    //           i++
-    //         ) {
-    //           console.log(constants.genders.donna.abbigliamento[i].name);
-    //           if (
-    //             macroCategory === constants.genders.donna.abbigliamento[i].name
-    //           ) {
-    //             ok = true;
-    //           }
-    //         }
-    //         if (!ok) {
-    //           throw new Error(
-    //             `la category ${macroCategory} non e' una category accetata`
-    //           );
-    //         }
-    //       }
-    //     }
-    //   }
   }
 };
 
