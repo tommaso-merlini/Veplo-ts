96bc7d788987a453a22d6e95670b9a53ae84219c
tommaso-merlini <tommaso.merlini.2004@gmail.com>
2023-05-10 16:20:47 +0200

    minor product query update

--- src/graphQL/user/queries/product/product.ts
+++ src/graphQL/user/queries/product/product.ts
@@ -1,13 +1,8 @@
 import { QueryProductArgs } from "src/graphQL/types/types.js";
 import productById from "../../../../controllers/queries/productById.js";
 
-export const product = async (
-  _: any,
-  { id }: QueryProductArgs,
-  { stripe }: any,
-  info: any
-) => {
-  const product = await productById(id);
+export const product = async (_: any, { id }: QueryProductArgs, info?: any) => {
+  const product = await productById(id, info);
 
   return product;
 };
