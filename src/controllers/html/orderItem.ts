0dfd0bb2f3c26193c47f3e15f740d8194be6d044
tommaso-merlini <tommaso.merlini.2004@gmail.com>
2023-06-07 16:13:56 +0200

    .

--- src/controllers/html/orderItem.ts
+++ src/controllers/html/orderItem.ts
@@ -6,7 +6,7 @@ export const orderItem = (item: ProductVariationsOrder) => {
         <tr>
             <td>
                 <div style="height: 100%; float: left; margin-top: 5px; margin-bottom: 5px; display: flex; align-items: center;">
-                    <img class="image-responsive" style="width: 110px; height: 110px; border-radius: 5px; margin-right: 10px;" src="https://loremflickr.com/100/100" alt="immagine non trovata" />
+                    <img class="image-responsive" style="width: 110px; height: 110px; border-radius: 5px; margin-right: 10px;" src=https://veplo-images.fra1.cdn.digitaloceanspaces.com/${item.photo}" alt="immagine non trovata" />
                     <div style="float: right;">
                         <p class="text-responsive" style="text-align: left; font-size: 16px; margin: 3px;  font-weight: 600;">${item.name} (${item.color})</p>
                         <p class="text-responsive" style="text-align: left; font-size: 14px; margin: 3px; font-weight: 400; color: #8F8F8F;">${item.brand}</p>
