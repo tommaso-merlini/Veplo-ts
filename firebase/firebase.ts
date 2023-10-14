64243dbbd16369eca637d59e0834b48d633ee0eb
Tommaso Merlini <83809589+tommaso-merlini@users.noreply.github.com>
2023-10-14 13:05:26 +0200

    Update firebase.ts

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -13,22 +13,22 @@ import admin from "firebase-admin";
 
 // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
 const dev_firebaseConfig = {
-  apiKey: "AIzaSyDs607C9zpuZjhM9H9lA48QRkFX4nZwi9o",
-  authDomain: "dintorni-dev.firebaseapp.com",
-  projectId: "dintorni-dev",
-  storageBucket: "dintorni-dev.appspot.com",
-  messagingSenderId: "519973926988",
-  appId: "1:519973926988:web:8570da91f33a1b6b2d9ce9",
-  measurementId: "G-FJ9863RJ7L",
+  apiKey: "<your-api-key>",
+  authDomain: "<your-auth-domain>",
+  projectId: "<your-project-id>",
+  storageBucket: "<your-storage-bucket-id>",
+  messagingSenderId: "<your-message-sender-id>",
+  appId: "<your-app-id>",
+  measurementId: "<your-measurement-id>",
 };
 const prod_firebaseconfig = {
-  apiKey: "AIzaSyD2Qy57T2kfvcmUXsgId8X2aUPJbnqvq34",
-  authDomain: "dintorni-prod.firebaseapp.com",
-  projectId: "dintorni-prod",
-  storageBucket: "dintorni-prod.appspot.com",
-  messagingSenderId: "890670080840",
-  appId: "1:890670080840:web:1c04dd1f4e6a29d4c5497d",
-  measurementId: "G-EETDDVBRRY",
+   apiKey: "<your-api-key>",
+  authDomain: "<your-auth-domain>",
+  projectId: "<your-project-id>",
+  storageBucket: "<your-storage-bucket-id>",
+  messagingSenderId: "<your-message-sender-id>",
+  appId: "<your-app-id>",
+  measurementId: "<your-measurement-id>",
 };
 
 let serviceAccount;
